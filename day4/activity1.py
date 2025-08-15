632df4aa80d909aae45e8581c2a7cb92104bacbf
GAURANG2809 <gaurang.budhija@gmail.com>
2025-08-15 15:58:22 +0400

    commit

---
+++ day4/activity1.py
@@ -0,0 +1,10 @@
+a=10
+b=30
+c=20
+if a>b and a>c:
+    print('a is greater than b and c')
+elif a<b or a>c:
+    print('a is less than b or a is greater than c')
+
+else:
+    print('not available ')
