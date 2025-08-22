ff8a939a05bf75b0237bd836261f58820ba915e2
GAURANG2809 <gaurang.budhija@gmail.com>
2025-08-22 16:17:30 +0400

    commit

---
+++ day5/activity3.py
@@ -0,0 +1,11 @@
+marks1=80
+marks2=90
+marks3=95
+marks4=99
+marks5=100
+total=marks1+marks2+marks3+marks4+marks5
+average=total/5
+if average >=90 and average <=100:
+    print('you grade is A1')
+else:
+    print('your grade is not A1')
